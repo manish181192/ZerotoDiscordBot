a03347a8cb7e62e27661bbb942300c04477166da
TyJK <TJKolody@gmail.com>
2019-06-25 18:42:06 -0400

    Update ReadingListBOT.py
    Added pinning, ability to enter in a discussion date and refactored the main block of the new_article function.

--- ReadingListBOT.py
+++ ReadingListBOT.py
@@ -8,6 +8,7 @@ test_server_link = "https://discord.gg/kN8vwQz"
 
 class DiscordBOT(discord.Client):
 
+
     async def on_ready(self):
         print('Logged in as')
         print(self.user.name)
@@ -23,7 +24,7 @@ class DiscordBOT(discord.Client):
                 else:
                     self.reading_list_groups[channel.category] = channel
 
-# TODO: set mods only, add date, make syntax more general (make double spaces acceptable), pin message to channel, zotero functionality
+# TODO: set mods only, make syntax more general (make double spaces acceptable)
 
     async def on_message(self, message: discord.Message):
         if message.author.id == self.user.id:
@@ -46,8 +47,10 @@ class DiscordBOT(discord.Client):
 
     async def bothelp(self, message):
         await message.channel.send(
-            "To add a current reading: @ReadingListBOT -new {title} {link}\n"
-            "To add a link to Zotero: @ReadingListBOT -zotero {link}. arxiv links are prefered.\n"
+            "To add a current reading: @ReadingListBOT -new {title} {link} {date}. Adding a discussion date is "
+            "optional. ReadingListBOT will then post a clean copy of your post, pin it, and post it in the appropriate"
+            "reading channels.\n"
+            # "To add a link to Zotero: @ReadingListBOT -zotero {link}. arxiv links are prefered.\n"
             "To edit a mistaken entry, edit the original entry and it will update automatically.\n"
             "New articles can't be posted from the general category or lobby channels")
 
@@ -63,27 +66,34 @@ class DiscordBOT(discord.Client):
             if len(text_in_brackets) == 2:
                 title = text_in_brackets[0]
                 link = text_in_brackets[1]
-                output = "Title: **{}**\nDiscussion Channel: {}\nLink: {}".format(title, message.channel.mention, link)
-
-                # this determines if it is a new message or not. the switch has to be inside this function to avoid
-                # cluttering the on_message and on_message_edit functions
-                try :
-                    children_messages = self.edit_log[message.id]
-                    if children_messages:
-                        new_local_message = await children_messages[0].edit(content=output)
-                        new_global_message = await children_messages[1].edit(content=output)
-                        self.edit_log[message.id] = [new_local_message, new_global_message]
-                    else:
-                        local_message = await self.reading_list_groups[message.channel.category].send(output)
-                        global_message = await self.general_reading_list.send(output)
-                        self.edit_log[message.id] = [local_message, global_message]
-                except:
-                    local_message = await self.reading_list_groups[message.channel.category].send(output)
-                    global_message = await self.general_reading_list.send(output)
-                    self.edit_log[message.id] = [local_message, global_message]
+                output = "Title: **{}**\nIn: {}\nLink: {}".format(title, message.channel.mention, link)
+                pin_output = "Title: **{}**\nLink: {}".format(title, link)
+            elif len(text_in_brackets) == 3:
+                title = text_in_brackets[0]
+                link = text_in_brackets[1]
+                date = text_in_brackets[2]
+                output = "Title: **{}**\nIn: {}\nLink: {}\nDiscussion Date: {}".format(title, message.channel.mention, link, date)
+                pin_output = "Title: **{}**\nLink: {}\nDiscussion Date: {}".format(title, link, date)
             else:
                 await message.channel.send("I don't understand. For instructions, post @readerbot -help")
                 self.edit_log[message.id] = []
+                return
+            # this determines if it is a new message or not. the switch has to be inside this function to avoid
+            # cluttering the on_message and on_message_edit functions
+
+            # checks if the message is present in the edit log, and if so that it's not empty (a broken post)
+            if message.id in self.edit_log and self.edit_log[message.id] != []:
+                children_messages = self.edit_log[message.id]
+                new_local_message = await children_messages[0].edit(content=output)
+                new_global_message = await children_messages[1].edit(content=output)
+                new_pin_message = await children_messages[2].edit(content=pin_output)
+                self.edit_log[message.id] = [new_local_message, new_global_message, new_pin_message]
+            else:
+                local_message = await self.reading_list_groups[message.channel.category].send(output)
+                global_message = await self.general_reading_list.send(output)
+                pin_message = await message.channel.send(pin_output)
+                await pin_message.pin()
+                self.edit_log[message.id] = [local_message, global_message, pin_message]
         else:
             await message.delete()
             await message.channel.send("Cannot post readings in the GENERAL category or lobbies. \n"
@@ -96,10 +106,8 @@ class DiscordBOT(discord.Client):
         # TODO Send an alert if the link is invalid or otherwise non-functional
         # TODO When posts are made from new categories and channels, add them as approrpriate
 
-
         pass
 
 
 client = DiscordBOT()
 client.run(token)
-
